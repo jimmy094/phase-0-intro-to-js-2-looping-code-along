9df819dca1d3761b70b4c0299a4bd032de64f9cf
jimmyalvarez <jimmyalvarezjr@gmail.com>
2021-03-26 12:49:00 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl!`);
+        
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = "surprise";
+
+
+function writeCards(names, event) {
+    let cards = []
+    for(let n = 0; n < names.length; n++) {
+    cards.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`);
+    
+    }
+    
+    return cards;
+}
+
+
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num);
+        num--;
+    }
+    
+}
